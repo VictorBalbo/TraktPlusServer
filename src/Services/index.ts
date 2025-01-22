d4b365200dfdcaa4e4fee641c575e856a26fbbcd
Victor Balbo <VBalbo@eagle.org>
2025-01-22 16:32:16 -0300

    Add index for services

---
+++ src/Services/index.ts
@@ -0,0 +1,5 @@
+import { MediaService } from './MediaService'
+import { TmdbService } from './TmdbService'
+import { TraktService } from './TraktService'
+
+export { MediaService, TmdbService, TraktService }
